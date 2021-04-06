519c4eeba7736c57a49e838964176af0887b12a5
Violet Forest <info@violetforest.com>
2021-04-06 12:54:03 +0200

    change grade properties for AAR

--- app/src/main/java/com/handapp/mediapipebluetooth/MediapipeFragment.java
+++ app/src/main/java/com/handapp/mediapipebluetooth/MediapipeFragment.java
@@ -16,7 +16,6 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ToggleButton;
 import android.content.SharedPreferences;
